a558d1fac807650b69c883c5cbbfbab5411c6090
rahulyer002 <ryer0003@student.monash.edu>
2026-04-16 21:58:06 +1000

    Bug fixes

--- FE/vue-app/vite.config.js
+++ FE/vue-app/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://16.26.250.110:8000',
+        target: 'https://connectlocal.duckdns.org',
         changeOrigin: true,
       },
     },
