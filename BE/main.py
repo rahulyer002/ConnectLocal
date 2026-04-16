71b30c75a2815a4750664faff816b605e0271a4f
rahulyer002 <ryer0003@student.monash.edu>
2026-04-16 22:10:03 +1000

    bug fixes for backend

--- BE/main.py
+++ BE/main.py
@@ -13,8 +13,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.get_allowed_origins(),
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
