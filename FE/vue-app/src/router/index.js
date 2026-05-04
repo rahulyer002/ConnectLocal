9886d6cae86c9a4ee6ed08e1f3a884409ecac092
rahulyer002 <ryer0003@student.monash.edu>
2026-05-04 17:01:02 +1000

    Refactoring and changing the iteration-1 to new UI/UX

--- FE/vue-app/src/router/index.js
+++ FE/vue-app/src/router/index.js
@@ -6,24 +6,17 @@ import CheckinPage from '../pages/CheckinPage.vue'
 import CheckinFormPage from '../pages/CheckinFormPage.vue'
 import ResultsPage from '../pages/ResultsPage.vue'
 import EventDetailsPage from '../pages/EventDetailsPage.vue'
-import AboutPage from '../pages/AboutPage.vue'
-import ResourcesPage from '../pages/ResourcesPage.vue'
 
 const router = createRouter({
   history: createWebHistory(),
-
   routes: [
     { path: '/', redirect: '/home' },
 
     { path: '/home', component: HomePage },
     { path: '/discover', component: DiscoverPage },
     { path: '/events/:id', component: EventDetailsPage },
-
-    { path: '/about', component: AboutPage },
-    { path: '/resources', component: ResourcesPage },
-
     { path: '/checkin', component: CheckinPage },
-    { path: '/checkin-form', component: CheckinFormPage },
+    { path: '/checkin/form', component: CheckinFormPage },
     { path: '/results', component: ResultsPage },
 
     { path: '/:pathMatch(.*)*', redirect: '/home' },
